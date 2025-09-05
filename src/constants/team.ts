763b3f3cf5b7c45bb36c4afe3648ac98ee6ced9e
zhekabaila <zheka.arkan@mimin.io>
2025-09-05 22:49:19 +0700

    feat: add about page

---
+++ src/constants/team.ts
@@ -0,0 +1,47 @@
+export const teamMembers = [
+  { name: 'Jeni Adi Hidayat', role: 'Ketua', image: '/team/jeni.png' },
+  {
+    name: 'Diva Marshelano AS',
+    role: 'Wakil Ketua',
+    image: '/team/diva.png',
+  },
+  {
+    name: 'Naurah Yulistia S.',
+    role: 'Bendahara 1',
+    image: '/team/naurah.png',
+  },
+  { name: 'Ristin Iman A.', role: 'Bendahara 2', image: '/team/ristin.png' },
+  { name: 'Alya Azhar N.', role: 'Sekretaris 1', image: '/team/alya.png' },
+  { name: 'Keumala Tri R.', role: 'Sekretaris 2', image: '/team/ken.png' },
+  { name: 'Gita Syafira A.', role: 'Koor. Humas', image: '/team/gita.png' },
+  { name: 'Raihana Afiyah', role: 'Staff Humas', image: '/team/najwa.png' },
+  { name: 'Wida Senja', role: 'Staff Humas', image: '/team/wida.png' },
+  {
+    name: 'Alfira Indah B.',
+    role: 'Staff Humas',
+    image: '/team/alifia.png',
+  },
+  {
+    name: 'Najwa Syafira N.S',
+    role: 'Koor. Media',
+    image: '/team/najwa.png',
+  },
+  { name: 'Dimas Putra M.', role: 'Staff Media', image: '/team/dimas.png' },
+  { name: 'Nazwa Aulia S.', role: 'Staff Media', image: '/team/nazwa.png' },
+  { name: 'Jovita Edgina', role: 'Staff Media', image: '/team/jovita.png' },
+  {
+    name: 'Rizqa Hany S.',
+    role: 'Koor. Research',
+    image: '/team/riqza.png',
+  },
+  {
+    name: 'Farid Firdaus',
+    role: 'Staff Research',
+    image: '/team/farid.png',
+  },
+  {
+    name: 'Ridho Awaludin',
+    role: 'Staff Research',
+    image: '/team/ridho.png',
+  },
+]
