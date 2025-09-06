82f5f532dc1cadd15aa3524cb58be5c126f979fb
shArdent <dmarshelano@gmail.com>
2025-09-06 10:57:38 +0700

    fix(main page): fix main landing page responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,45 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import type { Metadata } from 'next'
+import Image from "next/image";
+import Link from "next/link";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'Home - Photobooth by Siliwangi Code Developer (SICODEV)',
+  title: "Home - Photobooth by Siliwangi Code Developer (SICODEV)",
   description:
-    'Photobooth application developed by Siliwangi Code Developer (SICODEV) for event documentation purposes.',
-}
+    "Photobooth application developed by Siliwangi Code Developer (SICODEV) for event documentation purposes.",
+};
 
 export default function Home() {
   return (
-    <main className="flex justify-center items-center px-10 ">
-      <div className="relative w-full">
+    <main className="flex flex-col-reverse md:flex-row justify-center items-center px-10 ">
+      <div className="relative w-[80%] flex justify-center items-center">
+        {/* Gambar pertama */}
         <Image
-          className="-rotate-[15deg] relative left-40 top-10 z-9 [mask-image:linear-gradient(to_bottom,black,transparent)] [mask-repeat:no-repeat] [mask-size:100%_100%]"
-          src={'/LayoutB.svg'}
+          className="md:-rotate-[15deg] -rotate-[15deg] relative z-9 
+      [mask-image:linear-gradient(to_bottom,black,transparent)] 
+      [mask-repeat:no-repeat] [mask-size:100%_100%]"
+          src={"/LayoutB.svg"}
           alt="Photo layout"
           width={250}
           height={810}
         />
+
+        {/* Gambar kedua */}
         <Image
-          className="-rotate-3 absolute top-0 right-10 z-10"
-          src={'/LayoutB.svg'}
+          className="-rotate-3 absolute top-0 left-1/2 -translate-x-[20%] md:right-auto md:top-0 z-10"
+          src={"/LayoutB.svg"}
           alt="Photo layout"
           width={250}
           height={810}
         />
       </div>
-      <div className="flex flex-col justify-center items-center text-white text-center w-[160%] gap-7">
+
+      <div className="flex flex-col justify-center items-center text-white text-center w-full my-10 md:w-[160%] gap-7">
         <div>
           <h1 className="font-bold text-5xl mb-3 max-w-4xl leading-15">
-            Frame your{' '}
+            Frame your{" "}
             <i className="bg-gradient-to-r from-[#8F5CFF] to-[#3E08AB] bg-clip-text text-transparent">
               best moments
-            </i>{' '}
+            </i>{" "}
             and take a photo with us today.
           </h1>
           <p>
@@ -41,7 +47,7 @@ export default function Home() {
             special frame.
           </p>
         </div>
-        <Link href={'/capture'}>
+        <Link href={"/capture"}>
           <button
             type="button"
             className="bg-white text-[#3E08AB] cursor-pointer w-fit px-10 py-3 rounded-2xl font-bold"
@@ -51,5 +57,5 @@ export default function Home() {
         </Link>
       </div>
     </main>
-  )
+  );
 }
